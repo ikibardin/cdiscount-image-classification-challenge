97e058e9a10cfa989c99354a5b7ff77f1cea838b
Kibardin <ikibardin@gmail.com>
2017-10-16 17:10:04 +0300

    Fix dataset __len__()

--- scripts/loading.py
+++ scripts/loading.py
@@ -7,17 +7,19 @@ from torch.utils.data import Dataset
 class CdiscountDataset(Dataset):
     def __init__(self, img_ids, mode, transform=None):
         """
-        :param img_ids: list of tuples (product_id, img_number),
+        :param img_ids: np.array of tuples (product_id, img_number),
         img_number between 0 and 3
         """
         assert mode in ('train', 'test')
+        assert isinstance(img_ids, np.array)
         self._img_ids = img_ids
+        self._dataset_length = None
         self._transform = transform
         self._client = MongoClient(connect=False)
         self._db = self._client.cdiscount[mode]
 
     def __len__(self):
-        return len(self._img_ids)
+        return self._calculate_length()
 
     def __getitem__(self, item):
         product_id, image_number = self._img_ids[item]
@@ -27,6 +29,12 @@ class CdiscountDataset(Dataset):
             img = self._transform(img)
         label = product['category_id']
         return img, label
+
+    def _calculate_length(self):
+        if self._dataset_length is not None:
+            return self._dataset_length
+        self._dataset_length = self._img_ids[:, 1].sum() + self._img_ids.shape[0]
+        return self._dataset_length
 
     @staticmethod
     def _img_from_bytes(img_bytes):
