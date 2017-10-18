979f2e4792ce988017dbd505570c533e5586a43c
Avens8 <tsayipad@gmail.com>
2017-10-19 00:00:05 +0300

    Added option to build an id file with ids and category ids of each image

--- scripts/build_train_img_ids.py
+++ scripts/build_train_img_ids.py
@@ -6,7 +6,7 @@ from tqdm import tqdm
 import config
 
 
-def build_ids(mode):
+def build_ids(mode, with_cat_id=False):
     assert mode in ('train', 'test')
     res = list()
     client = MongoClient(connect=False)
@@ -14,11 +14,18 @@ def build_ids(mode):
     for product in tqdm(db.find(), total=config.PRODUCT_COUNT):
         assert 1 <= len(product['imgs']) <= 4
         for i in range(len(product['imgs'])):
-            res.append((product['_id'], i))
-    with open(config.IMG_IDS_PATH, 'wb') as f:
+            if with_cat_id:
+                res.append((product['_id'], i, product['category_id']))
+            else:
+                res.append((product['_id'], i))
+    if with_cat_id:
+        path = config.IMG_IDS_WITH_CAT_PATH
+    else:
+        path = config.IMG_IDS_PATH
+    with open(path, 'wb') as f:
         pickle.dump(res, f)
     print('Done.')
 
 
 if __name__ == '__main__':
-    build_ids('train')
+    build_ids('train', with_cat_id=True)
--- scripts/config.py
+++ scripts/config.py
@@ -3,3 +3,4 @@ CAT_COUNT = 5270
 CHECKPOINTS_DIR = '../checkpoints/'
 INCEPTION_V3_DIR = CHECKPOINTS_DIR + 'inception-v3/'
 IMG_IDS_PATH = '../input/img_ids.pickle'
+IMG_IDS_WITH_CAT_PATH = '../input/img_ids_with_cat.pickle'
