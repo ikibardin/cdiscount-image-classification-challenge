4e1fdcc7de37b18dc8f8aae7c72caaccdddd60d9
ikibardin <ikibardin@gmail.com>
2017-10-24 23:42:08 +0300

    Fix

--- scripts/config.py
+++ scripts/config.py
@@ -3,6 +3,7 @@ CHECKPOINTS_DIR = '../checkpoints/'
 
 INCEPTION_V3_DIR = CHECKPOINTS_DIR + 'inception-v3/'
 SEPARATOR_DIR = CHECKPOINTS_DIR + 'separator/'
+RESNET101 = CHECKPOINTS_DIR + 'resnet101/'
 
 IMG_IDS_PATH = '../input/img_ids.pickle'
 IMG_IDS_WITH_CAT_PATH = '../input/img_ids_with_cat.pickle'
--- scripts/train_resnet101.py
+++ scripts/train_resnet101.py
@@ -15,23 +15,22 @@ from tqdm import tqdm
 import config
 import loading
 from label_to_cat import LABEL_TO_CAT
-from mymodels import inception_v3_180
 
 BATCH_SIZE = 256
-EPOCHS = 50
-ITERS_PER_EPOCH = 45000
-VALID_SIZE = config.TRAIN_SIZE - ITERS_PER_EPOCH * BATCH_SIZE
+EPOCHS = 100
+VALID_SIZE = 851293
 
 PHASE_TRAIN = 'train'
 PHASE_VAL = 'val'
 
-# BEST_WEIGHTS = config.INCEPTION_V3_DIR + '18_epoch.pth'
 INITIAL_LR = 0.001
 
 
 def train():
-    all_imgs_ids = loading.load_separator_imgs_ids()
-    ids_train, ids_valid = train_test_split(all_imgs_ids, test_size=VALID_SIZE,
+    all_imgs_ids = loading.load_all_train_imgs_ids()
+    assert config.TRAIN_SIZE == len(all_imgs_ids)
+    ids_train, ids_valid = train_test_split(all_imgs_ids,
+                                            test_size=VALID_SIZE,
                                             random_state=0)
     print("Training on {} samples, validating on {} samples.".format(
         len(ids_train),
@@ -47,13 +46,13 @@ def train():
                                                       [0.485, 0.456, 0.406],
                                                       [0.229, 0.224, 0.225])
                                                   ]),
-                                             big_cats=True)
+                                             big_cats=False)
 
     train_loader = torch.utils.data.DataLoader(
         train_dataset,
         batch_size=BATCH_SIZE,
         shuffle=True,
-        num_workers=2
+        num_workers=1
     )
     valid_dataset = loading.CdiscountDataset(ids_valid, PHASE_TRAIN,
                                              transform=transforms.Compose(
@@ -65,12 +64,12 @@ def train():
                                                       [0.485, 0.456, 0.406],
                                                       [0.229, 0.224, 0.225])
                                                   ]),
-                                             big_cats=True)
+                                             big_cats=False)
     valid_loader = torch.utils.data.DataLoader(
         valid_dataset,
         batch_size=BATCH_SIZE,
         shuffle=True,
-        num_workers=2
+        num_workers=0
     )
     dataloaders = {
         PHASE_TRAIN: train_loader,
@@ -86,9 +85,9 @@ def train():
     model.fc = nn.Linear(num_ftrs, len(LABEL_TO_CAT))
     assert torch.cuda.is_available()
     # model.cuda()
-    model = nn.DataParallel(model, device_ids=[0, 1])
-    model.cuda()
+    model = nn.DataParallel(model, device_ids=[0, 1]).cuda()
     criterion = nn.CrossEntropyLoss()
+    criterion.cuda()
     optimizer = optim.SGD(model.parameters(), lr=INITIAL_LR, momentum=0.9)
     exp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
     model = train_model(model, dataloaders,
@@ -127,19 +126,14 @@ def train_model(model, dataloaders, dataset_sizes,
             running_corrects = 0
 
             # Iterate over data.
-            if phase == PHASE_TRAIN:
-                iternum = 0
-            total_iters = ITERS_PER_EPOCH if phase == PHASE_TRAIN else \
-                int(np.ceil(dataset_sizes[phase] / float(BATCH_SIZE)))
-
-            for data in tqdm(dataloaders[phase], total=total_iters):
+            for data in tqdm(dataloaders[phase], total=dataset_sizes[phase]):
                 # get the inputs
                 inputs, labels = data
 
                 # wrap them in Variable
                 assert torch.cuda.is_available()
                 inputs = Variable(inputs.cuda())
-                labels = Variable(labels.cuda())
+                labels = Variable(labels.cuda(async=True))
 
                 # zero the parameter gradients
                 optimizer.zero_grad()
@@ -157,29 +151,20 @@ def train_model(model, dataloaders, dataset_sizes,
                 # statistics
                 running_loss += loss.data[0]
                 running_corrects += torch.sum(preds == labels.data)
-                if phase == PHASE_TRAIN:
-                    iternum += 1
-                if phase == PHASE_TRAIN and iternum == ITERS_PER_EPOCH:
-                    break
-            if phase == PHASE_VAL:
-                epoch_loss = running_loss / dataset_sizes[phase]
-                epoch_acc = running_corrects / dataset_sizes[phase]
-                # print("Dividing by", dataset_sizes[phase])
-            else:
-                epoch_loss = running_loss / (iternum * BATCH_SIZE)
-                epoch_acc = running_corrects / (iternum * BATCH_SIZE)
-                # print("Dividing by", iternum * BATCH_SIZE)
+
+            epoch_loss = running_loss / dataset_sizes[phase]
+            epoch_acc = running_corrects / dataset_sizes[phase]
 
             print('{} Loss: {:.4f} Acc: {:.4f}'.format(
-                phase, epoch_loss, epoch_acc))
-
+                phase, epoch_loss, epoch_acc)
+            )
+            torch.save(model.state_dict(),
+                       config.RESNET101 + '{}_epoch.pth'.format(
+                           epoch))
             # deep copy the model
             if phase == PHASE_VAL and epoch_acc > best_acc:
                 best_acc = epoch_acc
                 best_model_wts = model.state_dict()
-                torch.save(best_model_wts,
-                           config.INCEPTION_V3_DIR + '{}_epoch_lower_lr.pth'.format(
-                               epoch))
                 print('Best weights updated!')
 
         print()
