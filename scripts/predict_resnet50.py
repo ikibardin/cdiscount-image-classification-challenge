ece31654e4fb911ab2f2cbdd7ead3802edd55ee4
ikibardin <ikibardin@gmail.com>
2017-11-20 16:21:14 +0300

    xD

--- scripts/predict_resnet50.py
+++ scripts/predict_resnet50.py
@@ -15,7 +15,7 @@ from probs_saver import ProbStore
 
 LOAD_WEIGHTS_FROM = config.RESNET50_DIR + '23_epoch_val.pth'
 
-TEST_BATCH_SIZE = 2048
+TEST_BATCH_SIZE = 256
 
 NORM_MEAN = [0.485, 0.456, 0.406]
 NORM_STD = [0.229, 0.224, 0.225]
@@ -23,7 +23,7 @@ NORM_STD = [0.229, 0.224, 0.225]
 
 def train():
     ids_test = pd.read_csv(config.TEST_IDS_PATH)
-    ids_test = ids_test[:TEST_BATCH_SIZE * 5]
+    ids_test = ids_test[:TEST_BATCH_SIZE]
     print('Predicting on {} samples.'.format(ids_test.shape[0]))
 
     test_dataset = loading.CdiscountDatasetPandas(
@@ -35,7 +35,7 @@ def train():
         test_dataset,
         batch_size=TEST_BATCH_SIZE,
         shuffle=True,
-        num_workers=7
+        num_workers=0
     )
 
     model = resnet50(pretrained=True, num_classes=config.CAT_COUNT)
@@ -54,7 +54,7 @@ def predict(model, dataloader, test_size):
     storage = ProbStore(path='../input/predict_probs_resnet50.h5')
     model.train(False)
     for data in tqdm(dataloader,
-                     total=int(np.ceil(test_size / float(TEST_BATCH_SIZE)))):
+                     total=np.ceil(test_size / float(TEST_BATCH_SIZE))):
         # get the inputs
         product_ids, image_numbers, inputs = data
         # wrap them in Variable
@@ -65,17 +65,22 @@ def predict(model, dataloader, test_size):
         bs, ncrops, c, h, w = inputs.size()
         assert bs == TEST_BATCH_SIZE and ncrops == 10
         outputs = model(inputs.view(-1, c, h, w))
-        proba = nn.functional.softmax(outputs.data)
+        proba = nn.functional.softmax(outputs.data).cpu()
 
-        new_ids = []
-        for x in product_ids:
-            new_ids += [x] * 10
-        new_numbers = []
-        for x in image_numbers:
-            new_numbers += [x] * 10
-
+        two_cols = []
+        for x, y in zip(product_ids, image_numbers):
+            two_cols += zip([x]*10, [y]*10)   
+        two_cols = np.array(two_cols)         
+#new_ids += [x] * 10
+            
+        #new_ids = np.transpose(new_ids)
+        #new_numbers = []
+        #for x in image_numbers:
+        #    new_numbers += [x] * 10
+        #new_numbers = np.transpose(new_numbers)
+        # print(two_cols.shape, proba.data.numpy().shape)
         df = pd.DataFrame(data=np.hstack(
-            [new_ids, new_numbers, proba.numpy()]
+            [two_cols, proba.data.numpy()]
         ), columns=columns, index=None)
         storage.saveProbs(df)
 
--- scripts/probs_saver.py
+++ scripts/probs_saver.py
@@ -13,14 +13,12 @@ class ProbStore:
         data -- a DataFrame for one image, index -- index of an image in test
         """
         assert isinstance(data, DataFrame)
-        self._hdf.put(str(self._length), data, table=True)
+        self._hdf.put('d' + str(self._length), data, table=True)
         self._length += 1
 
     def __len__(self):
         return self._length
 
     def __getitem__(self, index):
-        return self._hdf[str(index)]
+        return self._hdf['d' + str(index)]
 
-    def __del__(self):
-        self._hdf.close()
